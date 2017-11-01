d22e4277f37c1fcfe0fe7d321680671cffcde302
Steve Cho <supersteeb@gmail.com>
2017-11-01 20:00:26 +0700

    Fixing name

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,10 +2,10 @@ class Word < ApplicationRecord
   belongs_to :user
 
   validates :chinese, presence: true, uniqueness: {case_sensitive: false}
-	validates :meaning, presence: true
+	validates :english, presence: true
 
 	def self.search(search)
-		where("character LIKE ? OR meaning LIKE ? OR pinyin LIKE ? OR part_of_speech LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+		where("chinese LIKE ? OR english LIKE ? OR pinyin LIKE ? OR part_of_speech LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
 		#where("character LIKE ?", "%#{search}%")
 		#where("pinyin LIKE ?", "%#{search}%")
 		#where("part_of_speech LIKE ?", "%#{search}%")
